01d5af779fd71e132ff96bd8ae123f4d55911ca8
welhoilija <tkankas@gmail.com>
2019-12-24 17:32:54 +0200

    On branch master  Changes to be committed: 	modified:   pricecatch.py

--- pricecatch.py
+++ pricecatch.py
@@ -22,6 +22,9 @@ pygame.display.set_caption("BTCPRICE " + currency + " " + "last")
 pricesvar = 0
 
 font = pygame.font.Font('freesansbold.ttf', 100)
+font1 = pygame.font.Font('freesansbold.ttf', 80)
+font2 = pygame.font.Font('freesansbold.ttf', 60)
+font3 = pygame.font.Font('freesansbold.ttf', 40)
 screen.fill(white)
 oldvalue = 0
 
@@ -41,7 +44,7 @@ class Background(pygame.sprite.Sprite):
 
 BackGround = Background('gray.png', [0,0])
 
-Priceslist = []
+Priceslist = [0,0,0,0,0]
 
 while 1:
 	for event in pygame.event.get():
@@ -79,9 +82,21 @@ while 1:
 		textRect.center = (640 * 0.65, 480 * 0.85)
 
 
-		text1 = font.render(str(Priceslist[0]), True, color1, None) 
+		text1 = font1.render(str(Priceslist[0]), True, color1, None) 
 		text1Rect = text1.get_rect()
-		text1Rect.center = (640 * 0.65, 480 * 0.6)
+		text1Rect.center = (640 * 0.65, 480 * 0.65)
+
+
+
+		text2 = font2.render(str(Priceslist[1]), True, color2, None) 
+		text2Rect = text2.get_rect()
+		text2Rect.center = (640 * 0.65, 480 * 0.53)
+
+
+
+		text3 = font3.render(str(Priceslist[2]), True, color3, None) 
+		text3Rect = text3.get_rect()
+		text3Rect.center = (640 * 0.65, 480 * 0.44)
 
 		"""buytext = font.render(int(parsed_json["EUR"]["buy"]), True, green, None) 
 		buytextRect = buytext.get_rect()
@@ -95,8 +110,10 @@ while 1:
 		"""screen.blit(buytext, buytextRect)
 		screen.blit(selltext, selltextRect)"""
 		screen.blit(text1, text1Rect)
+		screen.blit(text2, text2Rect)
+		screen.blit(text3, text3Rect)
 		pygame.display.update()
-		time.sleep(30)
+		time.sleep(60)
 		#solid blocks
 	oldvalue = value 
 	color4 = color3
