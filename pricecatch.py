7ecd597eee905851a9909ea0e6ec4a87034d80f0
welhoilija <tkankas@gmail.com>
2019-12-26 01:17:52 +0200

    optimization

--- pricecatch.py
+++ pricecatch.py
@@ -9,10 +9,13 @@ import pygame, sys
 pygame.init()
 
 white = (255, 255, 255) 
-green = (0, 255, 0) 
 blue = (0, 0, 128) 
 black = (0, 0, 0)
-red = (255, 0, 0)
+red = (200,0,0)
+green = (0,200,0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
 currency = input("which currency would you like? f.e. 'EUR'")
 #last = input("which price would you like? f.e. 'last' or 'buy'")
 
@@ -22,16 +25,18 @@ pygame.display.set_caption("BTCPRICE " + currency + " " + "last")
 pricesvar = 0
 
 font = pygame.font.Font('freesansbold.ttf', 100)
-font1 = pygame.font.Font('freesansbold.ttf', 80)
-font2 = pygame.font.Font('freesansbold.ttf', 60)
-font3 = pygame.font.Font('freesansbold.ttf', 40)
+font1 = pygame.font.SysFont('ubuntu.ttf', 80)
+font2 = pygame.font.SysFont('ubuntu.ttf', 40)
+
 screen.fill(white)
 oldvalue = 0
-
+color = green
 color1 = red
 color2 = red
 color3 = red
 color4 = red
+oldtime = time.time() - 60
+value = 0
 
 
 class Background(pygame.sprite.Sprite):
@@ -40,6 +45,13 @@ class Background(pygame.sprite.Sprite):
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
+
+
+def values(value, x, y, font, color):
+	text = font.render(value, True, color, None) 
+	textRect = text.get_rect()
+	textRect.center = (x, y)
+	screen.blit(text, textRect)
 
 
 BackGround = Background('gray.png', [0,0])
@@ -52,74 +64,80 @@ while 1:
 			pygame.quit()
 			sys.exit()
 
-	prices = requests.get("https://blockchain.info/ticker")
-	jsontext = prices.text
-	parsed_json= json.loads(jsontext)
-	value = float(parsed_json[currency]["last"])
-	print(value)
+
+	mouse = pygame.mouse.get_pos()
+	#print(mouse)
+	
 
 
 
-	Priceslist.insert(0, oldvalue)
-
-	if value > oldvalue:
-		color = green
-	else:
-		color = red
-
-	#check if value has changed since last check
-	if oldvalue == value:
+	#checks if a minute has passed, if it has run the numbers again
+	if time.time() - oldtime > 60:
+		print("its been a minute")
+		prices = requests.get("https://blockchain.info/ticker")
+		jsontext = prices.text
+		parsed_json= json.loads(jsontext)
+		value = float(parsed_json[currency]["last"])
 		print(value)
-		time.sleep(30)
-	#if value has changed, fill the screen with blue and redraw
+		Priceslist.insert(0, value)
+
+		if value > oldvalue:
+			color = green
+		else:
+			color = red
+		if oldvalue == value:
+			print(value)
+		#if value has changed, fill the screen with blue and redraw
+
+
+			
+		else:
+			screen.fill([0, 0, 0])
+			screen.blit(BackGround.image, BackGround.rect)
+
+			values(str(Priceslist[0]), 640 * 0.65, 480 * 0.90, font, color)
+
+			values(currency + "/BTC", 640 * 0.15, 480 * 0.90, font2, green)
+
+			values(str(Priceslist[1]), 640 * 0.65, 480 * 0.68, font1, color1)
+
+			values(str(Priceslist[2]), 640 * 0.65, 480 * 0.54, font1, color2)
+
+			values(str(Priceslist[3]), 640 * 0.65, 480 * 0.40, font1, color3)
+
+
+
+			
+
+
+		oldtime = time.time()
+		oldvalue = value
+		color4 = color3
+		color3 = color2
+		color2 = color1
+		color1 = color
+
+
+
+	if 168 > mouse[0] > 19 and 98 > mouse[1] > 69:
+		pygame.draw.rect(screen, bright_green, (18, 68, 152, 29))
+		currency = "USD"
 	else:
-		screen.fill([0, 0, 0])
-		screen.blit(BackGround.image, BackGround.rect)
-		pricesvar = value
-
-		text = font.render(str(parsed_json[currency]["last"]), True, color, None) 
-		textRect = text.get_rect()
-		textRect.center = (640 * 0.65, 480 * 0.85)
-
-
-		text1 = font1.render(str(Priceslist[0]), True, color1, None) 
-		text1Rect = text1.get_rect()
-		text1Rect.center = (640 * 0.65, 480 * 0.65)
+		pygame.draw.rect(screen, green, (18, 68, 152, 29))
 
 
 
-		text2 = font2.render(str(Priceslist[1]), True, color2, None) 
-		text2Rect = text2.get_rect()
-		text2Rect.center = (640 * 0.65, 480 * 0.53)
 
 
 
-		text3 = font3.render(str(Priceslist[2]), True, color3, None) 
-		text3Rect = text3.get_rect()
-		text3Rect.center = (640 * 0.65, 480 * 0.44)
 
-		"""buytext = font.render(int(parsed_json["EUR"]["buy"]), True, green, None) 
-		buytextRect = buytext.get_rect()
-		buytextRect.center = (640 * 0.7, 480 * 0.15)
 
-		selltext = font.render(int(parsed_json["EUR"]["sell"]), True, red, None) 
-		selltextRect = selltext.get_rect()
-		selltextRect.center = (640 * 0.7, 480 * 0.85)"""
+	pygame.display.update()
+		
 
-		screen.blit(text, textRect)
-		"""screen.blit(buytext, buytextRect)
-		screen.blit(selltext, selltextRect)"""
-		screen.blit(text1, text1Rect)
-		screen.blit(text2, text2Rect)
-		screen.blit(text3, text3Rect)
-		pygame.display.update()
-		time.sleep(60)
-		#solid blocks
-	oldvalue = value 
-	color4 = color3
-	color3 = color2
-	color2 = color1
-	color1 = color
+
+
+
 
 	
 
